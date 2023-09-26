27dab11e85cee961b89bcac160b2872563c667c1
sandeependla <31530012+sandeependla@users.noreply.github.com>
2023-09-26 16:43:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("Hello, World this is my new git hub project for jenkins");
    }
 }
